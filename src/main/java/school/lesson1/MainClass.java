59fbe52909ebcbff90df1354b50a9f9cce0a11c6
Igor <po4talen@gmail.com>
2022-05-17 23:42:16 +0300

    First commit

--- src/main/java/school/lesson1/MainClass.java
+++ src/main/java/school/lesson1/MainClass.java
@@ -1,7 +1,11 @@
 package school.lesson1;
 
+/*First Java Program.
+
+ */
 public class MainClass {
-    public static void main(){
+    public static void main(String[] args) {
         System.out.printf("Hello");
     }
+
 }
