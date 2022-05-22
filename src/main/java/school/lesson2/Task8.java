16af7f685b497e83be122e9245087f06dbdb8f52
Igor <po4talen@gmail.com>
2022-05-22 21:22:30 +0300

    Home Work 2

--- src/main/java/school/lesson2/Task8.java
+++ src/main/java/school/lesson2/Task8.java
@@ -14,18 +14,14 @@ public class Task8 {
         printTwoDimArray(arr);
 
     }
-    public static void  fillDiagonal(int[][] array){
-     //   int value = 0;
+
+    public static void fillDiagonal(int[][] array) {
+        //   int value = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if(i==j){
+                if ((i == j) || ((i + j) == (array.length - 1))) {
                     array[i][j] = 1;
                 }
-//                if (i==0&&j==4) {
-//                    array[i][j] = 1;
-//                }
-
-
             }
         }
     }
