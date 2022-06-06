408edf522e71037db4ac7b51d34e0cd61aef14a3
Igor <po4talen@gmail.com>
2022-06-06 23:21:38 +0300

    HomeWork 6

---
+++ src/main/java/school/lesson6/Main.java
@@ -0,0 +1,46 @@
+package school.lesson6;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] arr = new String[4][4];
+        int resultat = 0;
+        String[][] arr2 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            resultat = method(arr2);
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(resultat);
+    }
+
+
+    static public int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < 4; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < i; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException();
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
---
+++ src/main/java/school/lesson6/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package school.lesson6;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException() {
+        super("Value of arrays can't be sum");
+    }
+}
