5a0661b05e8de227a2606f622d8151a80e0339f5
Igor <po4talen@gmail.com>
2022-06-06 23:24:09 +0300

    HomeWork 6

--- src/main/java/school/lesson6/Main.java
+++ src/main/java/school/lesson6/Main.java
@@ -3,7 +3,6 @@ package school.lesson6;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] arr = new String[4][4];
         int resultat = 0;
         String[][] arr2 = {
                 {"1", "2", "3", "4"},
